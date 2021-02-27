61b819edbddeb9c254349d2696bf15956e05332a
Alakhdhar <62402868+Alakhdhar@users.noreply.github.com>
2021-02-27 18:37:35 +0100

    Update enregistrement.php

--- enregistrement.php
+++ enregistrement.php
@@ -1,12 +1,13 @@
 <?php session_start() ?>
 <!DOCTYPE html>
 <head>
-  <title> YassMira! </title>
+  <title> Let's Plan ! </title>
   <meta charset ="utf-8" />
     <link rel="stylesheet" href="formulaire.css"/>
   </head>
 <body>
   <header>
+      <h1>Bienvenue sur Let's Plan</h1>
     <h3> Créer votre compte </h3>
   </header>
    <form action="enregistrement.php" method="POST" >
@@ -16,7 +17,7 @@
       </span>
        <span class="champ">
            <label for="prénom">Prénom</label>
-   	 <input type="text" name="prénom" placeholder="taper votre prenom ici " size="23"> </br>
+   	 <input type="text" name="prénom" placeholder="taper votre prénom ici " size="23"> </br>
        </span>
         <span class="champ">
            <label for="identifiant">Identifiant</label>
@@ -30,52 +31,49 @@
       <input type="submit" value="Envoyer" name="go">
       </span>
     </form>
-
   <?php
-     //connexion au serveur:
-     $connexion=mysqli_connect('localhost','phpmyadmin','alakh99','utilisateurs');
-
-     if (!$connexion) {
-        // echo "Pas de connexion au serveur " ;
-         exit ;
-     }
-     echo "connexion réussie! <br/> " ;
-     mysqli_set_charset($connexion, "utf8"); //pour que les caractères reçus soient codés en utf-8.
-     //requête:
-     if(  (isset($_POST["nom"]) && (isset($_POST["prénom"])&&(isset($_POST["identifiant"])&&(isset($_POST["mdp"])))))){
-       if(!empty($_POST["nom"])&&!empty($_POST["prénom"])&&!empty($_POST["identifiant"])&&!empty($_POST["mdp"])){
-         $lastname =  mysqli_real_escape_string($connexion , $_POST['nom']);
-         $firstname =  mysqli_real_escape_string($connexion , $_POST['prénom']);
-         $identifiant =  mysqli_real_escape_string($connexion , $_POST['identifiant']);
-         $mdp=password_hash($_POST["mdp"],PASSWORD_DEFAULT);
-         $req_pseudo="SELECT identifiant  FROM user WHERE identifiant='{$identifiant}'";
-         $res_pseudo= mysqli_query($connexion,$req_pseudo);
-         if(!$res_pseudo){
-            echo" requête incorrecte \n";
-            echo mysqli_error($connexion);
-         }
-         else {
-           $count    = mysqli_num_rows($res_pseudo);
-           if ($count==0){
-           $req= "INSERT INTO user (nom,prenom,identifiant,mdp) VALUES('$lastname','$firstname','$identifiant','$mdp')" ;
-           $resultat = mysqli_query ($connexion, $req ) ;
-           if(!$resultat){
-              echo" requête incorrecte \n";
-              echo mysqli_error($connexion);
-           }
-           header('Location:connexion.php');
-         }
-         else {
-           echo "ce pseudo existe déja";
-         }
-       }
-      }
-    else {
-      echo "vouus devez remplier tout";
-    }
+  //connexion au serveur:
+  $connexion= new PDO('mysql:host=localhost;dbname=utilisateurs;charset=utf8', 'root', 'root');
+  if (!$connexion) {
+      echo "Pas de connexion au serveur " ;
+      exit ;
   }
-mysqli_close($connexion);
-?>
-   <footer><h3> Pour vous connectez cliquez  <a href="connexion.php">ici</a></h3></footer>
+  //echo "connexion réussie! " ;
+  //requête:
+  if(  (isset($_POST["nom"]) && (isset($_POST["prénom"])&&(isset($_POST["identifiant"])&&(isset($_POST["mdp"])))))){
+      if(!empty($_POST["nom"])&&!empty($_POST["prénom"])&&!empty($_POST["identifiant"])&&!empty($_POST["mdp"])){
+          $lastname =   $_POST['nom'];
+
+          $firstname =  $_POST['prénom'];
+          $identifiant = $_POST['identifiant'];
+          $mdp=$_POST["mdp"];
+          $res_pseudo=$connexion->query("SELECT identifiant  FROM user WHERE identifiant='{$identifiant}'");
+
+          if(!$res_pseudo){
+              echo" requête incorrecte \n";
+              $res_pseudo->closeCursor();
+          } else {
+              $res_pseudo->closeCursor();
+              $count = mysqli_num_rows($res_pseudo);
+              if ($count==0){
+                  $resultat= $connexion->exec("INSERT INTO user (nom,prenom,identifiant,mdp) VALUES('$lastname','$firstname','$identifiant','$mdp')" ) or die(print_r($connexion->errorInfo()));
+                  if(!$resultat){
+                      echo" requête incorrecte \n";
+                      //echo mysqli_error($connexion);
+                  }
+                  $resultat->closeCursor();
+                  header('Location:connexion.php');
+              } else {
+                  echo "Ce pseudo existe déja";
+              }
+          }
+      } else {
+          echo "Vous devez tout remplir";
+      }
+  }
+     $connexion->closeCursor();
+  ?>
+
+   <footer><h3> Pour vous connectez cliquez sur <a href="connexion.php">ici</a></h3></footer>
 </body >
 </html >
