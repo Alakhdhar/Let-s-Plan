8bc793f763f54fb498c55076bbca21ad1539c6f5
Alakhdhar <62402868+Alakhdhar@users.noreply.github.com>
2021-03-18 02:24:25 +0100

    Update login.php

--- login.php
+++ login.php
@@ -44,8 +44,8 @@
             <div class="inner">
                 <h2>Connexion</h2>
                 <p>
-                    <a href="https://accounts.google.com/o/oauth2/v2/auth?scope=email&access_type=online&response_type=code&redirect_uri=<?= urlencode('http://localhost/Let-s-Plan/testMike/events.html') ?>&client_id=<?= GOOGLE_ID ?>">Se connecter via Google</a>
-                    <a href="http://localhost/Let-s-Plan/testMike/events.html">Se connecter via Facebook</a>
+                    <a href="https://accounts.google.com/o/oauth2/v2/auth?scope=email&access_type=online&response_type=code&redirect_uri=<?= urlencode('http://localhost/Let-s-Plan/events.html') ?>&client_id=<?= GOOGLE_ID ?>">Se connecter via Google</a>
+                    <a href="http://localhost/Let-s-Plan/events.html">Se connecter via Facebook</a>
                 </p>
                 <section>
                     <form method="post" action="events.html">
